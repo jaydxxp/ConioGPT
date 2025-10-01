7a70cca8a990f244fbff3906bb44b73803ed46b7
jaydxxp <jaydeepwaghaskar2005@gmail.com>
2025-10-02 00:21:27 +0530

    frontend connection with apis done also added some ui stuff

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -1,38 +1,52 @@
 "use client";
 
-import {
-	ChatInput,
-	ChatInputSubmit,
-	ChatInputTextArea,
-} from "@/components/ui/chat-input";
+import { ChatInput, ChatInputSubmit, ChatInputTextArea } from "@/components/ui/chat-input";
+import axios from "axios";
 import { useState } from "react";
-import { toast } from "sonner";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github.css"; 
+import remarkGfm from "remark-gfm";
 
+interface BackendResponse {
+  response: string;
+}
 export default function Chat() {
-	const [value, setValue] = useState("");
-	const [isLoading, setIsLoading] = useState(false);
+  const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState<BackendResponse | null>(null);
 
-	const handleSubmit = () => {
-		setIsLoading(true);
-		setTimeout(() => {
-			toast(value);
-			setIsLoading(false);
-		}, 1000);
-	};
+  const handleSubmit = () => {
+    setIsLoading(true);
 
-	return (
-		<div className="w-full h-full flex justify-center items-center">
-			<ChatInput
-				variant="default"
-				value={value}
-				onChange={(e) => setValue(e.target.value)}
-				onSubmit={handleSubmit}
-				loading={isLoading}
-				onStop={() => setIsLoading(false)}
-			>
-				<ChatInputTextArea placeholder="Type a message..." />
-				<ChatInputSubmit />
-			</ChatInput>
-		</div>
-	);
+    axios
+      .post(process.env.NEXT_PUBLIC_BackendURL || "", { prompt: value })
+      .then((res) => setResponse(res.data))
+      .catch((err) => console.error(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+	
+    <div className="w-full h-full flex justify-center items-center flex-col gap-3">
+		<div className="flex justify-start text-2xl font-mono text-amber-50 ">CONIOGPT</div>
+      <div className=" text-white w-170 h-150 rounded-2xl p-4 overflow-auto border border-amber-100 ">
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
+          {response?.response || ""}
+        </ReactMarkdown>
+      </div>
+
+      <ChatInput
+        variant="default"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onSubmit={handleSubmit}
+        loading={isLoading}
+        onStop={() => setIsLoading(false)}
+      >
+        <ChatInputTextArea placeholder="Type a message..." />
+        <ChatInputSubmit />
+      </ChatInput>
+    </div>
+  );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,47 @@
+"use client";
 
-export default function Home() {
-  
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import Link from "next/link";
+
+export default function TypewriterEffectSmoothDemo() {
+  const words = [
+    {
+      text: "Your",
+    },
+    {
+      text: "AI",
+    },
+    {
+      text: "Co-Creator",
+    },
+    {
+      text: "for",
+    },
+    {
+      text:"Social"
+    },
+    {
+      text:"Media"
+    },
+    {
+      text: "Magic",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    {
+      text:"✨"
+    }
+  ];
   return (
-    <div className="text-white">
-      This is the where the AI starts.
+    <div className="flex flex-col items-center justify-center h-[40rem]">
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <Link href={"/chat"}>
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm cursor-pointer">
+          Let's Start
+        </button>
+        </Link>
+      
+      </div>
     </div>
   );
 }
